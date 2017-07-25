4771dd9b25de15ab8f5e13ce42d8d0b55fe1f62a
cvidalgatica <cvidalgatica@gmail.com>
2017-07-25 00:06:41 -0400

    Pharmapress git desktop
    Desde git desktop

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	// previene sobrecarga de iframe de Youtube
+    $('.modal').each(function(){
+        var src = $(this).find('iframe').attr('src');
+
+        $(this).on('click', function(){
+
+            $(this).find('iframe').attr('src', '');
+            $(this).find('iframe').attr('src', src);
+
+        });
+    });
+});
